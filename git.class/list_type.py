6216a37ef4c180513d705b1f855ca23a2c567eec
harini272000 <105209949+harini272000@users.noreply.github.com>
2023-03-02 13:00:40 +0530

    list

---
+++ git.class/list_type.py
@@ -0,0 +1,60 @@
+l = [1, 2, 3, 4, 5, 6]
+l1 = type(l)
+print(l1)
+
+l = [1, 2, 34, 'thunder soft', 45]
+print(l)
+
+l = [1, 2, 34, 'thunder soft', 45]
+l1 = l[0]
+print(l1)
+
+l = [1, 2, 34, 'thunder soft', 45]
+l1 = l[-2]
+print(l)
+
+l = [1, 2, 34, 'thunder soft', 45]
+l1 = l[0:3]
+print(l1)
+
+l = [1, 2, 34, 'thunder soft', 45]
+l1 = l[0:4:2]
+print(l1)
+
+l = [1, 2, 34, 'thunder soft', 45]
+l1 = l[::-1]
+print(l1)
+
+l = [1, 2, 34, 'thunder soft', 45]
+l1 = l[:4]
+print(l1)
+
+l = [1, 2, 34, 'thunder soft', 45]
+l1 = dir(l)
+print(l1)
+
+l = [1, 2, 34, 'thunder soft', 45]
+l1 = l.append("harini")
+print(l)
+
+l = [1, 2, 34, 'thunder soft', 45]
+l1 = l.clear()
+print(l)
+
+l = [1, 2, 34, 'thunder soft', 45]
+l1 = l.copy()
+print(l1)
+
+l = [1, 2, 34, 'thunder soft', 45, 1]
+l1 = l.count(1)
+print(l1)
+
+l = [1, 2, 34, 'thunder soft', 45]
+l1 = l.count(4)
+print(l1)
+
+
+
+
+
+
