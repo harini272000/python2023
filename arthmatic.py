1e412edec99e47149c5775cca031744ca4341488
harini272000 <105209949+harini272000@users.noreply.github.com>
2023-03-07 11:27:28 +0530

    arithmetic_operators

---
+++ arthmatic.py
@@ -0,0 +1,92 @@
+print("Arithmetic operators")
+#ADD operator addition of two numbers.
+def add(n1,n2):
+    return(n1+n2)
+print(add(10,20))
+
+#SUB operator subtraction of two numbers.
+def sub(n1,n2):
+    return(n1-n2)
+print(sub(10,20))
+
+#mul operator multiplication of two numbers.
+def mul(n1,n2):
+    return(n1*n2)
+print(mul(10,20))
+
+#div operator division of two numbers.
+def div(n1,n2):
+    return(n1/n2)
+print(div(10,20))
+
+#modules operator moduler division of two numbers.
+def modules(n1,n2):
+    return(n1%n2)
+print(modules(10,20))
+
+#Exponentiation operator power of two numbers.
+def exp(n1,n2):
+    return(n1**n2)
+print(exp(10,20))
+
+#Floor division operator
+def flodiv(n1,n2):
+    return(n1//n2)
+print(flodiv(10,20))
+
+
+a=int(input("a value is :"))
+b=int(input("b vaqlue is:"))
+add=a+b
+print("addition is:",add)
+substraction =a-b
+print("substraction is:",substraction)
+product =a*b
+print("multiplication is:",product)
+division =a/b
+print("division is:",division)
+#Arithmetic Operations
+'''a=int(input('enter a number: '))
+b=int(input('enter a number: '))
+
+#Addition of two numbers
+add=a+b
+print('addition of two numbers is',add)
+print('addition of {0} and {1} is {2}'.format(a,b,add))
+
+#sub of two numbers
+sub=a-b
+print('substraction of two numbers is',sub)
+print('substraction of {0} and {1} is {2}'.format(a,b,sub))
+
+#product of two numbers
+product=a*b
+print('product of two numbers is',product)
+print('product of {0} and {1} is {2}'.format(a,b,product))
+
+#division operation
+div=a/b
+print('division is',div)'''
+
+
+
+'''a=float(input('enter a number: '))
+b=float(input('enter a number: '))
+#Addition of two numbers
+add=a+b
+print('addition of two numbers is',add)
+print('addition of {0} and {1} is {2}'.format(a,b,add))
+
+#sub of two numbers
+sub=a-b
+print('substraction of two numbers is',sub)
+print('substraction of {0} and {1} is {2}'.format(a,b,sub))
+
+#product of two numbers
+product=a*b
+print('product of two numbers is',product)
+print('product of {0} and {1} is {2}'.format(a,b,product))
+
+#division operation
+div=a/b
+print('division is',div)'''
