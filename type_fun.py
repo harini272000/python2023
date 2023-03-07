33a01bff9a77ce82ccfca5ca43ab2ea42203c486
harini272000 <105209949+harini272000@users.noreply.github.com>
2023-03-07 11:30:48 +0530

    type_function

---
+++ type_fun.py
@@ -0,0 +1,21 @@
+a = ("Geeks", "for", "Geeks")
+b = ["Geeks", "for", "Geeks"]
+c = {"Geeks": 1, "for":2, "Geeks":3}
+d = ("Hello World")
+e = 10.23
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+
+print(type([]) is list)
+
+print(type([]) is not list)
+
+print(type(()) is tuple)
+
+print(type({}) is dict)
+
+print(type({}) is not list)
