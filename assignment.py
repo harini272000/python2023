a9eb51a1aa512e05cf91974b5a6816b7330e8f09
harini272000 <105209949+harini272000@users.noreply.github.com>
2023-03-07 11:26:35 +0530

    assignment_operator

---
+++ assignment.py
@@ -0,0 +1,106 @@
+print("Assignment operators")
+#a is equal to number.
+def equal():
+    a=4
+    print(a)
+equal()
+
+# plus equal operator used a=a+3.
+def plequ():
+    a=4
+    a+=3
+    print(a)
+plequ()
+
+#minus equal operator used a=a-3.
+def miequ():
+    a=4
+    a-=4
+    print(a)
+miequ()
+
+#mulequal operator used a=a*3.
+def mulequ():
+    a=4
+    a*=2
+    print(a)
+mulequ()
+
+#div equal operator used a=a%3.
+def divequ():
+    a=4
+    a%=20
+    print(a)
+divequ()
+
+'''a = 3
+b = 5
+# a = a + b
+a += b
+print(a)'''
+
+# a = a - b
+'''a = 3
+b = 5
+a -= b
+print(a)'''
+
+# a = a * b
+'''a = 3
+b = 5
+a *= b
+print(a)'''
+
+# a = a / b
+'''a = 3
+b = 5
+a /= b
+print(a)'''
+
+# a = a % b
+'''a = 3
+b = 5
+a %= b
+print(a)'''
+
+# a = a // b
+'''a = 3
+b = 5
+a //= b
+print(a)'''
+
+# a = a ** b
+'''a = 3
+b = 5
+a **= b
+print(a)'''
+
+# a = a & b
+'''a = 3
+b = 5
+a &= b
+print(a)'''
+
+# a = a | b
+'''a = 3
+b = 5
+a |= b
+print(a)'''
+
+# a = a ^ b
+'''a = 3
+b = 5
+a ^= b
+print(a)'''
+
+# a = a >> b
+'''a = 3
+b = 5
+a >>= b
+print(a)'''
+
+# a = a << b
+'''a = 3
+b = 5
+a <<= b
+print(a)'''
