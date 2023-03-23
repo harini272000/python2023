21ac67af95c77c56a552a7e1c9b9a0a53114d1aa
harini272000 <105209949+harini272000@users.noreply.github.com>
2023-03-23 10:41:10 +0530

    String_slicing

---
+++ 21march_task.py
@@ -0,0 +1,67 @@
+
+'''s = "thunder soft"
+t=s[0]
+print(t)
+'''
+
+'''
+u= "Observed access blocked error message "
+e=u[0:12]
+print(e)
+
+d="harini"
+x=d[::]
+print(x)
+
+d="python"
+a=d[:]
+print(a)
+
+d="python"
+p=d[4:7]
+print(p)
+
+v="python"
+c=v[0::2]
+print(c)
+
+a = ("a", "b", "c", "d", "e", "f", "g", "h")
+x = slice(3, 5)
+print(a[x])
+
+a = ("a", "b", "c", "d", "e", "f", "g", "h")
+x = slice(0, 8, 3)
+print(a[x])
+
+b = "Hello,World!"
+print(b[-5:-2])
+
+a = "Hello"
+b = "World"
+c = a + " " + b
+print(c)
+
+q = 3
+i = 567
+p = 49.95
+m = "I want {} pieces of item {} for {} dollars."
+print(m.format(q, i, p))
+'''
+t = 'It\'s alright.'
+print(t)
+
+s = "This will insert one \\ (backslash)."
+print(s)
+
+newline = "Harini\nnarisetty"
+print(newline)
+
+carre = "Hello\rWorld"
+print(carre)
+
+
+
+
+
+
+
